a7466399bac43046c194b2bbbc68d4f2da273698
rrich-kray <rrich.kray@gmail.com>
2022-02-08 10:29:36 -0500

    added routes

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-
+const inputCheck = require('./utils/inputCheck') 
 const mysql = require('mysql2')
 const express = require('express')
 
@@ -26,9 +26,99 @@ app.get('/', (req, res) => {
     })
 })
 
-db.query('SELECT * FROM candidates', (err, rows) => { // row is the database query response. If there are no errors in the SQL query, then the err value is null
-    console.log(rows)
+//create a candidate
+// const sql = 'INSERT INTO candidates (id, first_name, last_name, industry_connected) VALUES (?,?,?,?)'
+// const params = [11, 'Ronald', 'Firbank', 1]
+
+// db.query(sql, params, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log((result))
+// })
+
+ 
+app.delete('/api/candidate/:id', (req, res) => {
+    const sql = "DELETE FROM candidates WHERE id = ?"
+    const params = [req.params.id]
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.statusMessage(400).json({ error: res.message })
+            return;
+        } else if (!result.affectedRows) {
+            res.json({
+                message: 'Candidate not found'
+            });
+        } else {
+            res.json({
+                message: 'deleted',
+                changes: result.affectedRows,
+                id: req.params.id
+            })
+        }
+    })
 })
+
+// Create a candidate
+app.post('/api/candidate', ({ body }, res) => { // use object destructuring to pull data out of the req.body object
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected')
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected) VALUE (?,?,?)`
+    const params = [body.first_name, body.last_name, body.industry_connected]
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message })
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body
+        })
+    })
+})
+
+
+app.get('/api/candidate/:id', (req, res) => {
+    const sql = `SELECT * FROM candidates WHERE id = ?`
+    const params = [req.params.id]
+    
+    db.query(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message })
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        })
+    })
+})
+
+
+
+// get all candidates
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`
+
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        })
+    })
+})
+
+// db.query('SELECT * FROM candidates', (err, rows) => { // row is the database query response. If there are no errors in the SQL query, then the err value is null
+//     console.log(rows)
+// })
 
 // Default response for any other request (Not Found)
 // This route must be last, or else it will override all other routes
